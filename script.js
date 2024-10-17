a09b1b8832577c5c3c235cabedde5f71c44592dd
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-10-17 15:32:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+var submitButton = document.getElementById('btn');
+var message = document.getElementById('output');
+
+submitButton.addEventListener(
+	'click',()=>{
+		const numberInput = parseInt(document.getElementById('delay').value);
+		returnTime(numberInput);
+	}
+);
+
+function getTime(numberInput)
+{
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			var textInput = document.getElementById('text').value;
+			resolve(textInput);
+		},numberInput)
+	})
+}
+
+async function returnTime(numberInput)
+{
+	try{
+		const ans = await getTime(numberInput);
+		message.textContent = ans;
+	}catch(error)
+	{
+		console.log(error);
+	}
+}
+
